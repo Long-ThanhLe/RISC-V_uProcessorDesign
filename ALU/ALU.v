d1ec42a0ce8c36ffdf241878b1908d0355f85415
long-lethanh <1611866@hcmut.edu.vn>
2020-06-14 16:51:03 +0700

    Ignore all unrelated files

---
+++ ALU/ALU.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/14/2020 04:36:35 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+
+    );
+endmodule
